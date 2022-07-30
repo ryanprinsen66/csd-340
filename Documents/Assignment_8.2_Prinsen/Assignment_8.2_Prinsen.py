2b61297bad9621cd52f9b3a742aae246a56453d4
Prinrya <ryan.prinsen66@gmail.com>
2022-07-30 15:29:39 -0500

    Project.

--- Documents/Assignment_8.2_Prinsen/Assignment_8.2_Prinsen.py
+++ Documents/Assignment_8.2_Prinsen/Assignment_8.2_Prinsen.py
@@ -1,8 +1,8 @@
 # This is a header for the application
 # You should read this header and insert your name and your date below as part of the peer review
 # This is a typical part of any program
-# Author: <author>
-# Creation Date: <date>
+# Author: <Ryan Prinsen>
+# Creation Date: <07/30/2022>
 # Below is a simple program with 10 issues (some are syntax errors and some are logic errors.  You need to identify the issues and correct them.
 
 import random
@@ -17,13 +17,18 @@ def displayIntro():
 
 def chooseCave():
     cave = ''
+#Indentation issue with the while statement below.
 	while cave != '1' and cave != '2':
+#I would suggest defining the variable with the input function. "cave = input('Which cave will...')
 		print('Which cave will you go into? (1 or 2)')
 		cave = input()
 
+#The variable below should be "cave".
 	return caves
 
+#It may be a good idea to space some of this code out to make it not so jumbled.
 def checkCave(chosenCave):
+#Indentation issues in the next several lines.
 	print('You approach the cave...')
 	#sleep for 2 seconds
 	time.sleep(2)
@@ -39,16 +44,21 @@ def checkCave(chosenCave):
 	if chosenCave == str(friendlyCave):
 		print('Gives you his treasure!')
 	else:
-		print 'Gobbles you down in one bite!'
+#Print statement is missing parentheses below.
+		print 'Gobbles you down in one bite!' 
 
 playAgain = 'yes'
+#The while statement should have two equal signs rather than one in the line below.
 while playAgain = 'yes' or playAgain = 'y':
 	displayIntro()
+#The line below should be chooseCave not choosecave.
 	caveNumber = choosecave()
 	checkCave(caveNumber)
     
+#Could also have playAgain = input("Do you want...").
 	print('Do you want to play again? (yes or no)')
 	playAgain = input()
 	if playAgain == "no":
+#The line below should say "playing" not planing.
 		print("Thanks for planing")
 
